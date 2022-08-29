49fd8a0c16a65b8e271518b9f63477b044e046a0
Daniel Little <daniel.t.little@vanderbilt.edu>
2022-08-29 16:16:46 -0500

    semicolons

--- sensorsystem.ino
+++ sensorsystem.ino
@@ -14,124 +14,124 @@ const int ANALOG_PIN = A0; // The only analog pin on the Thing
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 
-Adafruit_ADS1015 ads1015 // create an instance of the ADC
+Adafruit_ADS1015 ads1015; // create an instance of the ADC
 
 WiFiServer server(80);
 
-void setup() 
+void setup()
 {
-  initHardware();
-  setupWiFi();
-  server.begin();
+    initHardware();
+    setupWiFi();
+    server.begin();
 
-  ads1015.begin();  // Initialize ads1015 at the default address 0x48
+    ads1015.begin();  // Initialize ads1015 at the default address 0x48
 }
 
-void loop() 
+void loop()
 {
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
+    // Check if a client has connected
+    WiFiClient client = server.available();
+    if (!client) {
+        return;
+    }
 
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
+    // Read the first line of the request
+    String req = client.readStringUntil('\r');
+    Serial.println(req);
+    client.flush();
 
-  // Match the request
-  int val = -1; // We'll use 'val' to keep track of both the
-                // request type (read/set) and value if set.
-  if (req.indexOf("/led/0") != -1)
-    val = 0; // Will write LED low
-  else if (req.indexOf("/led/1") != -1)
-    val = 1; // Will write LED high
-  else if (req.indexOf("/read") != -1)
-    val = -2; // Will print pin reads
-  // Otherwise request will be invalid. We'll say as much in HTML
+    // Match the request
+    int val = -1; // We'll use 'val' to keep track of both the
+    // request type (read/set) and value if set.
+    if (req.indexOf("/led/0") != -1)
+        val = 0; // Will write LED low
+    else if (req.indexOf("/led/1") != -1)
+        val = 1; // Will write LED high
+    else if (req.indexOf("/read") != -1)
+        val = -2; // Will print pin reads
+    // Otherwise request will be invalid. We'll say as much in HTML
 
-  // Set GPIO5 according to the request
-  if (val >= 0)
-    digitalWrite(LED_PIN, val);
+    // Set GPIO5 according to the request
+    if (val >= 0)
+        digitalWrite(LED_PIN, val);
 
-  client.flush();
+    client.flush();
 
-  // Prepare the response. Start with the common header:
-  String s = "HTTP/1.1 200 OK\r\n";
-  s += "Content-Type: text/html\r\n\r\n";
-  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+    // Prepare the response. Start with the common header:
+    String s = "HTTP/1.1 200 OK\r\n";
+    s += "Content-Type: text/html\r\n\r\n";
+    s += "<!DOCTYPE HTML>\r\n<html>\r\n";
 
-  /*Note: Uncomment the line below to refresh automatically
-   *      for every 1 second. This is not ideal for large pages 
-   *      but for a simple read out, it is useful for monitoring 
-   *      your sensors and I/O pins. To adjust the fresh rate, 
-   *      adjust the value for content. For 30 seconds, simply 
-   *      change the value to 30.*/
-  //s += "<meta http-equiv='refresh' content='1'/>\r\n";//auto refresh page
+    /*Note: Uncomment the line below to refresh automatically
+     *      for every 1 second. This is not ideal for large pages
+     *      but for a simple read out, it is useful for monitoring
+     *      your sensors and I/O pins. To adjust the fresh rate,
+     *      adjust the value for content. For 30 seconds, simply
+     *      change the value to 30.*/
+    //s += "<meta http-equiv='refresh' content='1'/>\r\n";//auto refresh page
 
-  // If we're setting the LED, print out a message saying we did
-  if (val >= 0)
-  {
-    s += "LED is now ";
-    s += (val)?"on":"off";
-  }
-  else if (val == -2)
-  {
-      // TURBIDITY
-      int16_t adc0
-      adc0 = ads1015.readADC_SingleEnded(0);
-      float turbidity = adc0 * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+    // If we're setting the LED, print out a message saying we did
+    if (val >= 0)
+    {
+        s += "LED is now ";
+        s += (val)?"on":"off";
+    }
+    else if (val == -2)
+    {
+        // TURBIDITY
+        int16_t adc0;
+        adc0 = ads1015.readADC_SingleEnded(0);
+        float turbidity = adc0 * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
 
-      // SOIL MOISTURE
+        // SOIL MOISTURE
 
-      // If we're reading pins, print out those values:
-    s += "Turbidity = ";
-    s += String(turbidity);
-  }
-  else
-  {
-    s += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
-  }
-  s += "</html>\n";
+        // If we're reading pins, print out those values:
+        s += "Turbidity = ";
+        s += String(turbidity);
+    }
+    else
+    {
+        s += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
+    }
+    s += "</html>\n";
 
-  // Send the response to the client
-  client.print(s);
-  delay(1);
-  Serial.println("Client disonnected");
+    // Send the response to the client
+    client.print(s);
+    delay(1);
+    Serial.println("Client disonnected");
 
-  // The client will actually be disconnected 
-  // when the function returns and 'client' object is detroyed
+    // The client will actually be disconnected
+    // when the function returns and 'client' object is detroyed
 }
 
 void setupWiFi()
 {
-  WiFi.mode(WIFI_AP);
+    WiFi.mode(WIFI_AP);
 
-  // Do a little work to get a unique-ish name. Append the
-  // last two bytes of the MAC (HEX'd) to "Thing-":
-  uint8_t mac[WL_MAC_ADDR_LENGTH];
-  WiFi.softAPmacAddress(mac);
-  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
-                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
-  macID.toUpperCase();
-  String AP_NameString = "ESP8266 Thing " + macID;
+    // Do a little work to get a unique-ish name. Append the
+    // last two bytes of the MAC (HEX'd) to "Thing-":
+    uint8_t mac[WL_MAC_ADDR_LENGTH];
+    WiFi.softAPmacAddress(mac);
+    String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                   String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+    macID.toUpperCase();
+    String AP_NameString = "ESP8266 Thing " + macID;
 
-  char AP_NameChar[AP_NameString.length() + 1];
-  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+    char AP_NameChar[AP_NameString.length() + 1];
+    memset(AP_NameChar, 0, AP_NameString.length() + 1);
 
-  for (int i=0; i<AP_NameString.length(); i++)
-    AP_NameChar[i] = AP_NameString.charAt(i);
+    for (int i=0; i<AP_NameString.length(); i++)
+        AP_NameChar[i] = AP_NameString.charAt(i);
 
-  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+    WiFi.softAP(AP_NameChar, WiFiAPPSK);
 }
 
 void initHardware()
 {
-  Serial.begin(115200);
-  pinMode(DIGITAL_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
-  // Don't need to set ANALOG_PIN as input, 
-  // that's all it can be.
+    Serial.begin(115200);
+    pinMode(DIGITAL_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+    // Don't need to set ANALOG_PIN as input,
+    // that's all it can be.
 }
